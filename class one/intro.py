e77e9de80dc3db6851812d8251d51a05c56d2698
Khyati Akkiraju <khyati.akkiraju@gmail.com>
2026-01-03 11:12:30 -0700

    print+variable

--- class one/intro.py
+++ class one/intro.py
@@ -1,2 +1,22 @@
 print("hello")
-print("khyati")
+print(12)
+print("khyati \n" * 5)
+print(1, 2, 3, 4, 5)
+print("my name is khyati", 12,98.0)
+#arithmetic
+print("sum of 10 and 20 is", 10+20)
+print(f"sum of 10 and 20 is {10+20}")
+print(f"sub 20 and 10 is {20-10}")
+print(f"mul of 2 and 5 is {2*5}")
+
+
+print(f"""
+    first number : {number1}
+    second number : {number2}
+    sum : {number1 + number2}
+    sub : {number1 -number2}
+    mul : {number1 * number2}
+    div : {number1 / number2}
+    rem : {number1 + number2}
+    power : {number1 + number2}
+""")
